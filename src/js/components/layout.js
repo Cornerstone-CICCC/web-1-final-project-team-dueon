3f073d88db522881c5d828f65c1a16129f03b5a2
ellieCorner <ellie25corner@gmail.com>
2025-07-22 16:37:11 -0700

    feat: update layout script to apply header component across pages

--- src/js/components/layout.js
+++ src/js/components/layout.js
@@ -1,7 +1,32 @@
-export async function loadLayout() {
+function toRelativePath(path) {
+  const depth = location.pathname.split('/').length - 2;
+  return '../'.repeat(depth) + path.replace(/^\.\//, '');
+}
+
+async function loadHeader() {
   const headerHtml = await fetch('/src/components/header.html').then((res) =>
     res.text()
   );
+  const headerEl = document.createElement('div');
+  headerEl.innerHTML = headerHtml;
 
-  document.getElementById('header').innerHTML = headerHtml;
+  const tagsToFix = headerEl.querySelectorAll('[src], [href]');
+  tagsToFix.forEach((el) => {
+    if (el.hasAttribute('src')) {
+      el.setAttribute('src', toRelativePath(el.getAttribute('src')));
+    }
+    if (el.hasAttribute('href')) {
+      const href = el.getAttribute('href');
+
+      if (!href.startsWith('http')) {
+        el.setAttribute('href', toRelativePath(href));
+      }
+    }
+  });
+
+  document.getElementById('header').appendChild(headerEl);
+}
+
+export async function loadLayout() {
+  await loadHeader();
 }
