8cf72d47cf54f51a95d01737b0d0af2ed02ddbc0
ellieCorner <ellie25corner@gmail.com>
2025-07-25 00:24:10 -0700

    refactor: change the function method way (function -> class)

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
 import { ContactMethodController } from './components/contact.js';
 import { BookingFormController } from './components/forms.js';
-import { loadLayout } from './components/layout.js';
-import { initNavigation } from './components/navigation.js';
-import { setActiveLinks } from './utils/activeLink.js';
+import { LayoutLoader } from './components/layout.js';
+import { NavigationController } from './components/navigation.js';
+import { ActiveLinkController } from './utils/activeLink.js';
 
 async function init() {
-  await loadLayout();
-  initNavigation();
-  setActiveLinks();
-
+  const layoutLoader = new LayoutLoader();
+  await layoutLoader.loadLayout();
+  new NavigationController();
+  new ActiveLinkController();
   new ContactMethodController();
   new BookingFormController('#booking-form');
 }
