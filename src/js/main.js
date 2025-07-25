1fd8d7542c893db9874ba2908ad65920affc1998
ellieCorner <ellie25corner@gmail.com>
2025-07-25 00:32:30 -0700

    [#20] Home - Form section (#21)
    * feat: add assets for form section
    
    * feat: update styles and fix layout issues
    
    * feat: create common inout, checkbox, radio styles
    
    * feat: create form section (markup and style)
    
    * feat: create contact method and apply function to form
    
    * feat: implement email system
    
    * feat: update calendar function for form section
    
    * feat: vaildate phone number and handle format
    
    * feat: validate email
    
    * refactor: change the function method way (function -> class)
    
    * fix: include local number

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,16 @@
-import { loadLayout } from './components/layout.js';
-import { initNavigation } from './components/navigation.js';
-import { setActiveLinks } from './utils/activeLink.js';
+import { ContactMethodController } from './components/contact.js';
+import { BookingFormController } from './components/forms.js';
+import { LayoutLoader } from './components/layout.js';
+import { NavigationController } from './components/navigation.js';
+import { ActiveLinkController } from './utils/activeLink.js';
 
 async function init() {
-  await loadLayout();
-  initNavigation();
-  setActiveLinks();
+  const layoutLoader = new LayoutLoader();
+  await layoutLoader.loadLayout();
+  new NavigationController();
+  new ActiveLinkController();
+  new ContactMethodController();
+  new BookingFormController('#booking-form');
 }
 
 init();
